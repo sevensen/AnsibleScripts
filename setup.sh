dc38e8f953a9f3fc7ad5779a84db46a284f26c41
sevensen <sven@evensen.cc>
2016-02-17 21:19:39 +0100

    Added new scripts

---
+++ setup.sh
@@ -0,0 +1,9 @@
+export AWS_ACCESS_KEY_ID='AKIAIXNO2WPB6WK6HARQ'
+export AWS_SECRET_ACCESS_KEY='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'
+
+export EC2_INI_PATH=/etc/ansible/ec2.ini
+export ANSIBLE_HOSTS=/etc/ansible/ec2.py
+
+ssh-agent bash 
+ssh-add ~/.ssh/id_rsa
+
